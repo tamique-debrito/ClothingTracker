a0c0b2cada84ddcd0c4d90adbcd97f51b1d99206
Tamique de Brito <51467831+tamique-debrito@users.noreply.github.com>
2025-04-14 22:22:32 -0500

    Add a couple of unit tests

---
+++ Tests/Color/TestColor.cs
@@ -0,0 +1,33 @@
+﻿using ClothingTracker.Models.Shared;
+using Xunit;
+
+namespace ClothingTracker.Tests
+{
+    public class ColorMatcherTests
+    {
+        [Fact]
+        public void FindClosestEnumColor_ShouldReturnRed_ForFF3333()
+        {
+            // Arrange
+            var inputHex = "#ff3333";
+
+            // Act
+            var result = SimpleDiscreteColorExtensions.ClosestDiscreteColor(inputHex);
+
+            // Assert
+            Assert.Equal(SimpleDiscreteColor.Red, result);
+        }
+        [Fact]
+        public void FindClosestEnumColor_ShouldReturnBlue_For3355AA()
+        {
+            // Arrange
+            var inputHex = "#ff3333";
+
+            // Act
+            var result = SimpleDiscreteColorExtensions.ClosestDiscreteColor(inputHex);
+
+            // Assert
+            Assert.Equal(SimpleDiscreteColor.Red, result);
+        }
+    }
+}
